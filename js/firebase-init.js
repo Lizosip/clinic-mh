f3c1e22b81c8d264d5a90860a373fd2cdc259be7
Lizosip <119516558+Lizosip@users.noreply.github.com>
2026-06-17 02:58:57 +0300

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,1 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyA_K95yFhHJaku4AUl5ntF-qEN9owOtrhs",
+    authDomain: "clinicmh-3012c.firebaseapp.com",
+    projectId: "clinicmh-3012c",
+    storageBucket: "clinicmh-3012c.firebasestorage.app",
+    messagingSenderId: "801576443044",
+    appId: "1:801576443044:web:1d97bc437a4a5931589a31"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
